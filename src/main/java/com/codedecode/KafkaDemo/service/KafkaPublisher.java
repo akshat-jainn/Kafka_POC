e5138c73e3100398ade4f85b6fc8d89ac7e0b572
Akshat Jain <akshat.jain@aurigait.com>
2023-04-28 23:23:56 +0530

    1st commit

---
+++ src/main/java/com/codedecode/KafkaDemo/service/KafkaPublisher.java
@@ -0,0 +1,44 @@
+package com.codedecode.KafkaDemo.service;
+
+import com.codedecode.KafkaDemo.configuration.KafkaTopicConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Service
+public class KafkaPublisher {
+
+	@Autowired
+	KafkaTopicConfig kafkaTopicConfig;
+
+	@Autowired
+	private KafkaTemplate<String,String> kafkaTemplate;
+
+	public void sendMessage(String msg)
+	{
+		ListenableFuture<SendResult<String,String>> future = kafkaTemplate.
+				send(
+						kafkaTopicConfig.topic().name(),
+						msg
+				);
+
+		future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+			@Override
+			public void onFailure(Throwable ex) {
+				System.out.println("Unable to send message ERROR : "+ex.getMessage());
+			}
+
+			@Override
+			public void onSuccess(SendResult<String, String> result) {
+				System.out.println("Sent message=[" + msg +
+						"] with offset=[" + result.getRecordMetadata().offset() + "]");
+			}
+		});
+	}
+}
